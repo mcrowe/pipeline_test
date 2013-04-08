d2a2baa7225adb34424b4f253e071ece24a24643
Mitch Crowe <crowe.mitch@gmail.com>
2013-04-08 09:19:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PipelineTest::Application.config.secret_token = '929693e9f5797e25fd584ca7ad14cbde191cb6cd5dccb06829b3980d015cbb92b5e50c36974451f7caa3679ecd3601c9ab563b85e9043b99442f8d005ac0bc39'
